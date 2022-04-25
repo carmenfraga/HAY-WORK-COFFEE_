0e9a9c2433ef7162403a47865a929824cc2d4384
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-04-25 17:47:43 +0200

    aportacion de mierda

--- hay-work-coffee/routes/auth.routes.js
+++ hay-work-coffee/routes/auth.routes.js
@@ -1,8 +1,29 @@
+// const { post } = require("./coffees.routes");
+
 const router = require("express").Router();
-router.get("/log-in", (req, res, next) => {
-    res.render("auth/log-in");
-});
+
+
+
+
 router.get("/sign-up", (req, res, next) => {
     res.render("auth/sign-up");
 });
+
+router.post("/sign-up", (req, res, next) => {
+    
+    console.log('The form data: ', req.body);
+
+})
+
+
+
+
+
+
+router.get("/log-in", (req, res, next) => {
+    res.render("auth/log-in");
+});
+
+router.post('/')
+
 module.exports = router;
---
+++ hay-work-coffee/routes/user.routes.js
@@ -0,0 +1,11 @@
+// const router = require('express').Router()
+
+// const { isLoggedIn } = require('./../middleware/route-guard')
+
+// router.get('/profile', isLoggedIn, (req, res) => {
+//     res.render('user/profile', { user: req.session.currentUser })
+// })
+
+// module.exports = router
+
+
