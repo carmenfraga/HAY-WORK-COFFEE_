eb63021ab4e62c3c946fcade1a1693d271c5db25
Iñigo Malluguiza <malluguizamae@gmail.com>
2022-04-28 17:05:02 +0200

    terminado google de maps

--- hay-work-coffee/public/js/coffees-map.js
+++ hay-work-coffee/public/js/coffees-map.js
@@ -1,8 +1,10 @@
 let map
+let geocoder
 
 function initMap() {
     renderMap()
     getCoffees()
+
 }
 
 function renderMap() {
@@ -13,29 +15,31 @@ function renderMap() {
         document.querySelector('#coffeesMap'),
         {
             center: { lat: 40.392499, lng: -3.698214 },
-            zoom: 10,
+            zoom: 17,
         }
     )
 }
 
 function getCoffees() {
-
-    axios.get('/api/coffees')
+    axios
+        .get('/api/coffees')
         .then(({ data }) => coffeeMarkers(data))
         .catch(err => console.log(err))
 }
 
 function coffeeMarkers(coffees) {
-
     const { Marker } = google.maps
 
     coffees.forEach(coffee => {
 
-        const position = {
-            lat: coffee.location.coordinates[0],
-            lng: coffee.location.coordinates[1],
-        }
-
-        new Marker({ position, title: coffee.name, map })
+        new Marker({
+            map, position: {
+                lat: coffee.address.location.coordinates[0],
+                lng: coffee.address.location.coordinates[1]
+            },
+            title: coffee.name
+        })
     })
 }
+
+
--- hay-work-coffee/routes/coffees.routes.js
+++ hay-work-coffee/routes/coffees.routes.js
@@ -6,24 +6,21 @@ const Comment = require('./../models/Comment.model')
 
 const { isLoggedIn, checkRole } = require('./../middlewares/route-guard')
 
-
 router.get('/coffees/new', (req, res, next) => {
 
-    Coffee
-        .find()
-        .then(coffees => {
-            res.render('coffees/new-coffee', { coffees })
-        })
-        .catch(err => next(err))
+    res.render('coffees/new-coffee')
 })
 
 router.post('/coffees/new', fileUploader.single('coffeeImage'), (req, res, next) => {
 
     const { name, address, latitude, longitude } = req.body
     const { path } = req.file
+    lat = Number(latitude)
+    lng = Number(longitude)
+    let location = [lat, lng]
 
     Coffee
-        .create({ name, image: path, address: { address, location: { type: "Point", coordinates: [latitude, longitude] } } })
+        .create({ name, image: path, address: { address, location: { type: "Point", coordinates: location } } })
         .then(() => {
             res.redirect('/coffees')
         })
@@ -31,8 +28,10 @@ router.post('/coffees/new', fileUploader.single('coffeeImage'), (req, res, next)
 })
 
 router.get('/coffees', (req, res, next) => {
+
     Coffee
         .find()
+        .select('name image')
         .then(coffees => {
             res.render('coffees/coffees', { coffees })
         })
@@ -40,7 +39,6 @@ router.get('/coffees', (req, res, next) => {
 });
 
 router.get('/coffees/:id', isLoggedIn, (req, res, next) => {
-
 
     const { id } = req.params
 
@@ -56,8 +54,6 @@ router.get('/coffees/:id', isLoggedIn, (req, res, next) => {
         .all(promises)
         .then(([coffeeRes, experiencesRes]) => {
 
-
-
             const comments = experiencesRes.map(eachExperience => {
                 return Comment.find({ experience: eachExperience._id })
             })
@@ -72,19 +68,11 @@ router.get('/coffees/:id', isLoggedIn, (req, res, next) => {
             viewInfo.experiences = viewInfo.experiences.map((eachExperience, idx) => {
                 return { ...eachExperience._doc, comments: allComments[idx] }
             })
-            // console.log('LA VIEWINFO ---->', viewInfo)
-            // res.json(viewInfo)
 
-            // res.render('coffees/coffee-details', viewInfo)
-            res.render('coffees/coffee-details', {viewInfo, isAdmin})
+            res.render('coffees/coffee-details', { viewInfo, isAdmin })
         })
 
-        // console.log('LOS COMENTARIOS------------------>', comments)
-        // res.render('coffees/coffee-details', { coffeeRes, experiencesRes, commentsMapped })
-        // console.log('LA DEL CAFE', coffeeRes, 'LA DE LAS EXPERIENCIAS', experiencesRes)
         .catch(err => next(err))
-
-
 })
 
 router.post('/coffees/:id/delete', checkRole('ADMIN'), (req, res, next) => {
@@ -104,7 +92,6 @@ router.post('/coffees/:id/delete', checkRole('ADMIN'), (req, res, next) => {
 router.get('/coffees/:id/edit', checkRole('ADMIN'), (req, res, next) => {
 
     const { id } = req.params
-
 
     Coffee
         .findById(id)
